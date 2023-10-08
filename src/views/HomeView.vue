9be494a428c681260ae75f3d1e262784206f65d4
NATTAYAx <boongna01@gmail.com>
2023-10-08 10:15:55 +0700

    new data file

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <Carousel_View />
+  <Toptrending_View />
+  <Art_list/>
+  <Game_list/>
+  <Pro_list/>
+</div>
+</template>
+
+<script setup>
+import Toptrending_View from '@/components/Toptrending_View.vue'
+import Carousel_View from '@/components/Carousel_View.vue'
+import Art_list from '@/data/nft.js'
+import Game_list from '@/data/nft.js'
+import Pro_list from '@/data/nft.js'
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
